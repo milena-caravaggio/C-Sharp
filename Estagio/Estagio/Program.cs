ceb3250f7197a443098f5517886df51bb2dadb9b
Milena Lima Caravaggio Acquesta <48251038+milena-caravaggio@users.noreply.github.com>
2020-01-30 10:21:37 -0300

    atualizado fibonacci

---
+++ Estagio/Estagio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Estagio
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroAnterior = 0;
+            int numeroAtual = 1;
+            int ateNumero;
+            int fibonacci;
+
+
+            Console.WriteLine("Informe o numero maximo para o Fibonacci");
+            ateNumero = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                fibonacci = numeroAnterior + numeroAtual;
+                Console.WriteLine(fibonacci);
+                numeroAnterior = numeroAtual;
+                numeroAtual = fibonacci;
+            }
+
+            while (fibonacci <= ateNumero);
+
+        }
+    }
+}
