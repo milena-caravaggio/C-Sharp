b8aba910f00c37c9aa18937e6838a62cc8a8390c
Milena Lima Caravaggio Acquesta <milena.acquesta@gmail.com>
2020-05-01 11:45:40 -0300

    Refatoração dos códigos

--- EquacaoPrimeiroGrau/EquacaoPrimeiroGrau/Program.cs
+++ EquacaoPrimeiroGrau/EquacaoPrimeiroGrau/Program.cs
@@ -8,47 +8,32 @@ namespace EquacaoPrimeiroGrau
         public static void Main(string[] args)
         {
             double a, b, c;
-            Console.WriteLine("Programa para resolver equação de primeiro grau como no exemplo: ax + b = c ");
+            Console.WriteLine("Programa para resolver equação de primeiro grau como no exemplo: Ax + B = C ");
 
-            Console.WriteLine("Digite o valor de 'a':");
-            a = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine($"{GetSignal(a)} ax  b = c");
-            Console.WriteLine("Agora digite o valor de 'b': ");
-            b = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine($"{a}x {GetSignal(b)} {b}b = c");
-            Console.WriteLine("Digite o valor do resultado 'c':");
-            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o valor de 'A':");
+            a = GetValue();
+
+            Console.WriteLine("Agora digite o valor de 'B': ");
+            b = GetValue();
+
+            Console.WriteLine("Digite o valor do resultado 'C':");
+            c = GetValue();
             Console.WriteLine($"Sua equação ficou assim {GetSignal(a)} {a} x {GetSignal(b)} {b} = {c}");
-           
 
-            double result;
-                  
-            
-            result = c - b;
-            result = result / a;
-            Console.WriteLine("O resultado é:" + result);
-           
+            double result = c - b;
+            result /= a;
+            Console.WriteLine("O resultado de X é: " + Math.Round(result, 4));
+
         }
-        
+
+        private static double GetValue()
+        {
+            return Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+        }
+
         private static string GetSignal(double b)
         {
             return (b.ToString().StartsWith("-") ? string.Empty : "+");
-        }
-
-        public static double equacaoMenos(double a, double b, double c)
-        {
-            double result = c + b;
-            result = result / a;
-            Console.WriteLine(result);
-            return result;
-        }
-
-        private static double equacaoMais(int a, int b, int c)
-        {
-            double result = c - b;
-            result = result / a;
-            Console.WriteLine(result);
-            return result;
         }
     }
 }
